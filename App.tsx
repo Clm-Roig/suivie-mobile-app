75a27a87dbe7e622fb8006aafc2082951f62953a
Clément Roig <clm.roig@gmail.com>
2022-07-19 08:58:13 +0200

    update icons

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { Dimensions, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
 
 export default function App() {
   return (
     <WebView
       source={{ uri: "https://clm-roig.github.io/suivie" }}
       style={{
-        marginTop: 20,
+        marginTop: Constants.statusBarHeight,
       }}
     />
   );
